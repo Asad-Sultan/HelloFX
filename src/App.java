aa8f84381972cd2bfc27d64109fa00de9b407e28
Asad Sultan <76148295+Asad-Sultan@users.noreply.github.com>
2024-02-26 15:29:14 +0500

    Added .fxml for integration with SceneBuilder

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 public class App extends Application {
@@ -10,7 +11,7 @@ public class App extends Application {
 
   @Override
   public void start(Stage myStage) throws Exception {
-    Group root = new Group();
+    Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
     Scene scene = new Scene(root);
 
     myStage.setScene(scene);
